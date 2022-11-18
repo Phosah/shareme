1c2b331bb1e725f2cdb0835dd04ab7387c4cefbd
Phosah <efosa.uyi@gmail.com>
2022-11-18 19:43:47 +0100

    Init

---
+++ shareme_backend/schemas/postedBy.js
@@ -0,0 +1,6 @@
+export default {
+  name: "postedBy",
+  title: "PostdBy",
+  type: "reference",
+  to: [{ type: "user" }],
+};
---
+++ shareme_frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="62210376256-a28dt6026v2pt1kohn9q75enimfr34s6.apps.googleusercontent.com">
+      <Router>
+        <App />
+      </Router>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
